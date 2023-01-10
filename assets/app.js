f88336ff794c4cffdb78bf79680d2cb96ac25ed9
JordanR94 <44776172+JordanR94@users.noreply.github.com>
2023-01-10 15:37:35 +0000

    video 9 cleaned preview

--- assets/app.js
+++ assets/app.js
@@ -15,8 +15,12 @@ import { Modal } from 'bootstrap';
 
 document.addEventListener('turbo:before-cache', (event) => {
     if (document.body.classList.contains('modal-open')){
-        const modal = Modal.getInstance(document.querySelector('.modal'));
+        const modalEl = document.querySelector('.modal');
+        const modal = Modal.getInstance(modalEl);
+        modalEl.classList.remove('fade');
+        modal._backdrop._config.isAnimated = false;
         modal.hide();
+        modal.dispose();
     }
 })
 
