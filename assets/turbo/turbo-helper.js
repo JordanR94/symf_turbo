ffd960bf5ec49473b90a128e96073b9e3f1b890d
JordanR94 <44776172+JordanR94@users.noreply.github.com>
2023-04-19 16:16:35 +0100

    video 17 - transition finish

--- assets/turbo/turbo-helper.js
+++ assets/turbo/turbo-helper.js
@@ -16,18 +16,7 @@ const TurboHelper = class {
             document.body.classList.add('turbo-loading');
         });
 
-        document.addEventListener('turbo:before-render', () => {
-            // when we are about to render, start us faded out
-           event.detail.newBody.classList.add('turbo-loading');
-        });
-
-        document.addEventListener('turbo:render', () => {
-            // after rendering, we first allow the turbo-loading class to set the low opacity
-            // THEN, one frame later, we remove the turbo-loading class, which allows the fade in
-            requestAnimationFrame(() => {
-               document.body.classList.remove('turbo-loading');
-            });
-        });
+        this.initializeTransistions();
     }
 
     closeModal(){
@@ -55,6 +44,43 @@ const TurboHelper = class {
     }
     initializeWeatherWidget() {
         __weatherwidget_init();
+    }
+
+    isPreviewRendered() {
+        return document.documentElement.hasAttribute('data-turbo-preview');
+    }
+
+    initializeTransistions(){
+        document.addEventListener('turbo:before-render', (event) => {
+            if (this.isPreviewRendered()) {
+                // this is a preview that has been instantly swapped
+                // remove .turbo-loading so the preview starts fully opaque
+                event.detail.newBody.classList.remove('turbo-loading');
+                // start fading out 1 frame later after opacity starts full
+                requestAnimationFrame(() => {
+                    document.body.classList.add('turbo-loading');
+                });
+            } else {
+                const isRestoration = event.detail.newBody.classList.contains('turbo-loading');
+                if (isRestoration) {
+                    // this is a restoration (back button). Remove the class
+                    // so it simply starts with full opacity
+                    event.detail.newBody.classList.remove('turbo-loading');
+                    return;
+                }
+                // when we are *about* to render a fresh page
+                // we should already be faded out, so start us faded out
+                event.detail.newBody.classList.add('turbo-loading');
+            }
+        });
+
+        document.addEventListener('turbo:render', () => {
+            // after rendering, we first allow the turbo-loading class to set the low opacity
+            // THEN, one frame later, we remove the turbo-loading class, which allows the fade in
+            requestAnimationFrame(() => {
+                document.body.classList.remove('turbo-loading');
+            });
+        });
     }
 }
 
