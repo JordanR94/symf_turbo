cd9cf90226257574508896d73f708bbc1cfcb54c
JordanR94 <44776172+JordanR94@users.noreply.github.com>
2023-04-19 15:33:45 +0100

    video 16 transistions

--- assets/turbo/turbo-helper.js
+++ assets/turbo/turbo-helper.js
@@ -10,6 +10,24 @@ const TurboHelper = class {
         document.addEventListener('turbo:render', () => {
             this.initializeWeatherWidget();
         });
+
+        document.addEventListener('turbo:visit', () => {
+            //fade out the old body
+            document.body.classList.add('turbo-loading');
+        });
+
+        document.addEventListener('turbo:before-render', () => {
+            // when we are about to render, start us faded out
+           event.detail.newBody.classList.add('turbo-loading');
+        });
+
+        document.addEventListener('turbo:render', () => {
+            // after rendering, we first allow the turbo-loading class to set the low opacity
+            // THEN, one frame later, we remove the turbo-loading class, which allows the fade in
+            requestAnimationFrame(() => {
+               document.body.classList.remove('turbo-loading');
+            });
+        });
     }
 
     closeModal(){
