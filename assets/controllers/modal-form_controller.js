af3de46318ab1f0611d2d46baeb0b05dbb903cae
JordanR94 <44776172+JordanR94@users.noreply.github.com>
2023-05-16 15:09:35 +0100

    video36 modal redirect

--- assets/controllers/modal-form_controller.js
+++ assets/controllers/modal-form_controller.js
@@ -1,29 +1,36 @@
 import { Controller } from 'stimulus';
 import { Modal } from 'bootstrap';
+import * as Turbo from '@hotwired/turbo';
 
 export default class extends Controller {
     static targets = ['modal'];
     modal = null;
 
     connect() {
-        document.addEventListener('turbo:before-fetch-response', (event) => {
-            console.log(event);
-            if(!this.modal || !this.modal._isShown){
-                return;
-            }
+        this.boundBeforeFetchResponse = this.beforeFetchResponse.bind(this);
+        document.addEventListener('turbo:before-fetch-response', this.boundBeforeFetchResponse)
+    }
 
-            const fetchResponse = event.detail.fetchResponse;
-
-            if(fetchResponse.succeeded && fetchResponse.redirected){
-                event.preventDefault();
-                this.modal.hide();
-            }
-        });
+    disconnect() {
+        document.removeEventListener('turbo:before-fetch-response', this.boundBeforeFetchResponse)
     }
 
     async openModal(event) {
         this.modal = new Modal(this.modalTarget);
         this.modal.show();
+    }
+
+    beforeFetchResponse(event){
+        if(!this.modal || !this.modal._isShown){
+            return;
+        }
+
+        const fetchResponse = event.detail.fetchResponse;
+
+        if(fetchResponse.succeeded && fetchResponse.redirected){
+            event.preventDefault();
+            Turbo.visit(fetchResponse.location);
+        }
     }
 
 }
