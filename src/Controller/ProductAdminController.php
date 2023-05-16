91ed19b72ce1dd7fde37e5883ea984663932a7c7
JordanR94 <44776172+JordanR94@users.noreply.github.com>
2023-05-16 12:46:58 +0100

    video 33 modal cleanup

--- src/Controller/ProductAdminController.php
+++ src/Controller/ProductAdminController.php
@@ -57,6 +57,7 @@ class ProductAdminController extends AbstractController
         return $this->renderForm('product_admin/' . $template, [
             'product' => $product,
             'form' => $form,
+            'formTarget' => $request->headers->get('Turbo-Frame', '_top')
         ]);
     }
 
@@ -84,6 +85,7 @@ class ProductAdminController extends AbstractController
         return $this->render('product_admin/edit.html.twig', [
             'product' => $product,
             'form' => $form->createView(),
+            'formTarget' => $request->headers->get('Turbo-Frame', '_top')
         ], new Response(null, $form->isSubmitted() && !$form->isValid() ? 422 : 200));
     }
 
